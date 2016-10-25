a2c9ae0dd41f85950a0e54887ee68f1dcd4fdff6
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-10-25 13:31:49 -0200

    First release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'loggable-decorator',
   packages = ['loggable'],
-  version = '0.1',
+  version = '1.0.0',
   description = 'Add a logger attribute to class decorated',
   author = 'Peter Downs',
   author_email = 'peterldowns@gmail.com',
   url = 'https://github.com/ateliedocodigo/python-loggable-decorator',
-  download_url = 'https://github.com/ateliedocodigo/python-loggable-wrapper/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/ateliedocodigo/python-loggable-decorator/tarball/1.0.0',
   keywords = ['logging'],
   classifiers = [],
 )

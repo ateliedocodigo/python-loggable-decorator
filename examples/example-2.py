3fc92e41e5a80bc1fe3edb209aee55f03df19813
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-10-26 10:36:15 -0200

    Add loggable_class decorator

---
+++ examples/example-2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import logging
+import sys
+
+# this is just an example config
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG
+)
+
+from loggable import loggable_class as loggable
+
+@loggable
+class Example2(object):
+    def decorated_method_one(self):
+        self.logger.debug("Debug decorated_method_with_name")
+        print("This is a decorated method")
+
+    def decorated_method_two(self):
+        self.logger.debug("Debug decorated_method_two")
+        print("This is a decorated method")
+
+if __name__ == "__main__":
+    Example2().decorated_method_one()
+    Example2().decorated_method_two()

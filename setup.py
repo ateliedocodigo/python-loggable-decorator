08a7c9bdbc819379273a090f3e916aa965c2b3a7
Luis Fernando Gomes <dev@luiscoms.com.br>
2016-11-07 12:01:17 -0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'loggable-decorator',
   packages = ['loggable'],
-  version = '1.1.0',
+  version = '1.1.1',
   description = 'Add a logger attribute to class decorated',
   author = 'Luis Fernando Gomes',
   author_email = 'luiscoms@ateliedocodigo.com.br',
   url = 'https://github.com/ateliedocodigo/python-loggable-decorator',
-  download_url = 'https://github.com/ateliedocodigo/python-loggable-decorator/tarball/1.1.0',
+  download_url = 'https://github.com/ateliedocodigo/python-loggable-decorator/tarball/1.1.1',
   keywords = ['logging'],
   classifiers = [],
 )

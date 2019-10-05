75ad1b2cff50d2c7a4a3aac969eb32cb7f737882
Manish <itzmanish108@gmail.com>
2019-10-05 16:12:43 +0000

    Added: testcase for python3, travis config, Next commit will be for test case support on python2

---
+++ test/test_loggable.py
@@ -0,0 +1,21 @@
+import sys
+from unittest import TestCase
+
+from loggable import loggable_class, loggable_method
+
+
+@loggable_class
+class LoggableClassTestCase(TestCase):
+
+    def test_loggable(self):
+        with self.assertLogs(__name__, level="DEBUG") as ctx:
+            self.logger.debug("Debug Test for class")
+            self.assertEqual(ctx.output,['DEBUG:test.test_loggable:Debug Test for class'])
+
+class LoggableMethodTestCase(TestCase):
+
+    @loggable_method(__name__)
+    def test_loggable_method(self):
+        with self.assertLogs(__name__, level="DEBUG") as logs:
+            self.logger.debug("Debug Test for method")
+            self.assertEqual(logs.output, ['DEBUG:test.test_loggable:Debug Test for method'])

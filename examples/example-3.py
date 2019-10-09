15cc5b8bf8dcfde3a074b3c53f2001330f5812c7
Karol Czeryna <k@e-dot.uk>
2019-10-09 20:13:59 +0100

    Fix flake8 linting problems

--- examples/example-3.py
+++ examples/example-3.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from loggable import loggable
 
-class Example1(object):
 
+class Example1(object):
     @loggable
     def decorated_method_with_name(self):
         self.logger.debug("Debug decorated_method_with_name")
         print("This is a decorated method")
+
 
 @loggable
 class Example2(object):
@@ -17,6 +18,7 @@ class Example2(object):
     def decorated_method_two(self):
         self.logger.debug("Debug decorated_method_two")
         print("This is a decorated method")
+
 
 if __name__ == "__main__":
     Example1().decorated_method_with_name()

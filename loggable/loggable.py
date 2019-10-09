16de6965d964ac4014ba1de7d90a932e17d7e8cc
Karol Czeryna <k@e-dot.uk>
2019-10-09 19:18:57 +0100

    Fix flake8 issues

--- loggable/loggable.py
+++ loggable/loggable.py
@@ -16,18 +16,23 @@ def loggable_method(name):
     """
     Wraps a method to set logger on self.logger
     """
+
     def dec(func):
         @wraps(func)
         def wrapper(*args, **kw):
             try:
                 _self = args[0]
-                _self.logger = logging.getLogger(name if isinstance(name, str) else _self.__module__)
+                _self.logger = logging.getLogger(
+                    name if isinstance(name, str) else _self.__module__
+                )
             except Exception as e:
                 pass
             result = func(*args, **kw)
             return result
+
         return wrapper
-    if hasattr(name, '__call__'):
+
+    if hasattr(name, "__call__"):
         return dec(name)
     return dec
 

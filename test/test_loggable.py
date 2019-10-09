255b40c0508f76db3c681a7eb1da87fbbb55e097
Pratik Gandhi <pgandhi@splunk.com>
2019-10-09 22:54:55 +0530

    Adding single decorator for class and method

--- test/test_loggable.py
+++ test/test_loggable.py
@@ -1,7 +1,7 @@
 import sys
 from unittest import TestCase
 
-from loggable import loggable_class, loggable_method
+from loggable import loggable_class, loggable_method, loggable
 
 
 @loggable_class
@@ -16,6 +16,22 @@ class LoggableMethodTestCase(TestCase):
 
     @loggable_method(__name__)
     def test_loggable_method(self):
+        with self.assertLogs(__name__, level="DEBUG") as logs:
+            self.logger.debug("Debug Test for method")
+            self.assertEqual(logs.output, ['DEBUG:test.test_loggable:Debug Test for method'])
+
+@loggable
+class LoggableClassTestCase2(TestCase):
+
+    def test_loggable2(self):
+        with self.assertLogs(__name__, level="DEBUG") as ctx:
+            self.logger.debug("Debug Test for class")
+            self.assertEqual(ctx.output,['DEBUG:test.test_loggable:Debug Test for class'])
+
+class LoggableMethodTestCase(TestCase):
+
+    @loggable(__name__)
+    def test_loggable_method2(self):
         with self.assertLogs(__name__, level="DEBUG") as logs:
             self.logger.debug("Debug Test for method")
             self.assertEqual(logs.output, ['DEBUG:test.test_loggable:Debug Test for method'])
